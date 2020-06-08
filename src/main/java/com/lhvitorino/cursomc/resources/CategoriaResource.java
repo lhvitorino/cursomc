d1b7d2b042a31522b55053f60007785ef0b898b7
Luiz Henrique Vitorino <lhvitorino@gmail.com>
2020-06-08 15:43:02 -0300

    Criando repository e service para Categoria

--- src/main/java/com/lhvitorino/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lhvitorino/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,14 @@
 package com.lhvitorino.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lhvitorino.cursomc.domain.Categoria;
+import com.lhvitorino.cursomc.services.CategoriaService;
 
 /* Controlador REST e usado CTRL + SHIFT + o para importar biblioteca  */
 @RestController
@@ -19,20 +20,18 @@ import com.lhvitorino.cursomc.domain.Categoria;
 
 public class CategoriaResource {
 	
+	@Autowired
+	private CategoriaService service;
+	
 	/* Método de teste */
 	/* Associar aos verbos HTTP para ser uma função REST */
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1,"Informática");
-		Categoria cat2 = new Categoria(1,"Escritório");
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
 	}
 
 }
---
+++ src/main/java/com/lhvitorino/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.lhvitorino.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lhvitorino.cursomc.domain.Categoria;
+import com.lhvitorino.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+		
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+				
+	}
+
+}
