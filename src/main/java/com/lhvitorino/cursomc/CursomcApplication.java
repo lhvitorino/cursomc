85bebc90ac0c2f6d7ed025257b4ea0794e9a814c
Luiz Henrique Vitorino <lhvitorino@gmail.com>
2020-06-09 23:23:22 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/lhvitorino/cursomc/CursomcApplication.java
+++ src/main/java/com/lhvitorino/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.lhvitorino.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.lhvitorino.cursomc.domain.Cidade;
 import com.lhvitorino.cursomc.domain.Cliente;
 import com.lhvitorino.cursomc.domain.Endereco;
 import com.lhvitorino.cursomc.domain.Estado;
+import com.lhvitorino.cursomc.domain.Pagamento;
+import com.lhvitorino.cursomc.domain.PagamentoComBoleto;
+import com.lhvitorino.cursomc.domain.PagamentoComCartao;
+import com.lhvitorino.cursomc.domain.Pedido;
 import com.lhvitorino.cursomc.domain.Produto;
+import com.lhvitorino.cursomc.domain.enums.EstadoPagamento;
 import com.lhvitorino.cursomc.domain.enums.TipoCliente;
 import com.lhvitorino.cursomc.repositories.CategoriaRepository;
 import com.lhvitorino.cursomc.repositories.CidadeRepository;
 import com.lhvitorino.cursomc.repositories.ClienteRepository;
 import com.lhvitorino.cursomc.repositories.EnderecoRepository;
 import com.lhvitorino.cursomc.repositories.EstadoRepository;
+import com.lhvitorino.cursomc.repositories.PagamentoRepository;
+import com.lhvitorino.cursomc.repositories.PedidoRepository;
 import com.lhvitorino.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,7 +44,11 @@ public class CursomcApplication implements CommandLineRunner{
 	private EnderecoRepository enderecoRepository;
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+		
 		
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -86,6 +98,23 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pgto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pgto1);
+		
+		Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pgto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pgto1, pgto2));
+		
 		
 	}
 
